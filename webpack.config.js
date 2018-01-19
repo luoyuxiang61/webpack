3fd9855d2bcc1a41f237b52c6f83735263854c5c
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-19 18:05:17 +0800

    bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path')
-
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    modules: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 }
